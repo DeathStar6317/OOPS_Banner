3ae63d43607808f90c3b3edd3f3959ae95c90797
DeathStar6317 <DeathStar6317@users.noreply.github.com>
2026-02-17 15:06:02 +0530

    UC1: Print literal OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print literal text "OOPS" to console
+ *
+ * @author Saumil
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method – Entry point of Java application
+     */
+    public static void main(String[] args) {
+
+        // Print literal text OOPS
+        System.out.println("OOPS");
+
+    }
+}
